b27a883730e7765f8543b7f4a224e88187284f69
zemenu <zemenumekiria@gmail.com>
2024-07-05 22:50:25 +0300

    backend setup started

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+
+app.get('/api/test', async (req:Request, res:Response) =>{
+    res.json({message:"hello from express endpoint!"
+    });
+});
+
+
+app.listen(5000, () => {
+    console.log("server running on localhost: 5000")
+})
